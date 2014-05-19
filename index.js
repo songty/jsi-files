59645a2911fe4d55bf5ba66c8e577aa51dba9a03
Tian Song <tian.y.song@gmail.com>
2014-05-19 15:36:05 -0700

    implemented small compare

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var program = require('commander');
+var newArray  = [];
 
 module.exports.readCheck = function (filePath) {
 	fs.readFile(filePath,
@@ -9,15 +10,26 @@ module.exports.readCheck = function (filePath) {
 
 };
 
-module.exports.compare = function (array1, array2) {
+var compare = function (array1, array2) {
 	var sharedWords = [];
-	var longer, shorter;
+	var longer, shorter = [];
 	if (array1.length > array2.length) {
 		longer = array1;}
 	else {
 		longer = array2;
 	}
-	for (var i = 0; i < longer.length; i++) {
-		if (longer[i] === shorter[])
+	smallCompare(longer.forEach(), shorter);
+	// for (var i = 0; i < longer.length; i++) {
+	// 	if (longer[i] === shorter[]);
+	// };
+};
+
+var smallCompare = function (word, array) {
+	for ( var i = 0; i < array.length; i++){
+		if (word === array[i]){
+	 		newArray.push(word);
+		}
 	}
 };
+
+compare(['hellow', 'world'],['world','joe']);
