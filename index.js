9de8783f673309e2466bc342a814fa1597921f2f
Dmitry White <white.dmitry@gmail.com>
2014-05-19 15:50:37 -0700

    Functional, but defer to Tian's commit at this stage

--- index.js
+++ index.js
@@ -10,20 +10,6 @@ module.exports.readCheck = function (filePath) {
 
 };
 
-var compare = function (array1, array2) {
-	var sharedWords = [];
-	var longer, shorter = [];
-	if (array1.length > array2.length) {
-		longer = array1;}
-	else {
-		longer = array2;
-	}
-	smallCompare(longer.forEach(), shorter);
-	// for (var i = 0; i < longer.length; i++) {
-	// 	if (longer[i] === shorter[]);
-	// };
-};
-
 var smallCompare = function (word, array) {
 	for ( var i = 0; i < array.length; i++){
 		if (word === array[i]){
@@ -32,4 +18,25 @@ var smallCompare = function (word, array) {
 	}
 };
 
-compare(['hellow', 'world'],['world','joe']);
+var compare = function (array1, array2) {
+	var sharedWords = [];
+	var longer, shorter = [];
+	if (array1.length > array2.length) {
+		longer = array1;
+		shorter = array2;}
+	else {
+		longer = array2;
+		shorter = array1;
+	}
+	longer.forEach(function (word) {
+		smallCompare(word, shorter);
+	})
+	// for (var i = 0; i < longer.length; i++) {
+	// 	if (longer[i] === shorter[]);
+	// };
+};
+
+
+
+compare(['hellow', 'world', 'same'],['world', 'same', 'joe']);
+console.log(newArray);
