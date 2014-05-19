1b9c15c3b9602220d2c9522efbb5967b162bca19
Tian Song <tian.y.song@gmail.com>
2014-05-19 13:52:43 -0700

    setting up the project with tests

---
+++ index.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+var program = require('commander');
+
+// module.exports.readCheck = fs.readFile(program.args[0], { encoding: 'utf8' }, function(err, contents) {
+//   console.log(contents);
+// });
--- test/tests.js
+++ test/tests.js
@@ -2,10 +2,11 @@ var chai = require('chai');
 var expect = chai.expect;
 var fs = require('fs');
 var path = require('path');
+var _ = require('../index');
 
 describe('files', function() {
   it('can read fixtures', function(done) {
-    var file = path.join(__dirname, 'fixtures', 'one.txt');
+    var file = path.join(__dirname, 'fixtures', 'test-file1.txt');
     fs.readFile(file, { encoding: 'utf8' }, function(err, contents) {
       expect(err).to.not.exist;
       expect(contents).to.eql('hello world\n');
