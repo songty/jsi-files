2e95d716971283520ff848a63857545991f94328
Tian Song <tian.y.song@gmail.com>
2014-05-19 15:50:49 -0700

    it works

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var program = require('commander');
-var newArray  = [];
+var sharedWords = [];
 
 module.exports.readCheck = function (filePath) {
 	fs.readFile(filePath,
@@ -10,26 +10,32 @@ module.exports.readCheck = function (filePath) {
 
 };
 
+var smallCompare = function (word, array) {
+	for ( var i = 0; i < array.length; i++){
+		if (word === array[i]){
+	 		sharedWords.push(word);
+		}
+	}
+};
+
 var compare = function (array1, array2) {
-	var sharedWords = [];
 	var longer, shorter = [];
 	if (array1.length > array2.length) {
-		longer = array1;}
-	else {
+		longer = array1;
+		shorter = array2;
+	} else {
 		longer = array2;
+		shorter = array1;
 	}
-	smallCompare(longer.forEach(), shorter);
+	longer.forEach(function (word) {
+		smallCompare(word, shorter);
+	});
 	// for (var i = 0; i < longer.length; i++) {
 	// 	if (longer[i] === shorter[]);
 	// };
 };
 
-var smallCompare = function (word, array) {
-	for ( var i = 0; i < array.length; i++){
-		if (word === array[i]){
-	 		newArray.push(word);
-		}
-	}
-};
 
-compare(['hellow', 'world'],['world','joe']);
+
+compare(['hellow','you', 'tian', 'world'],['tian','world','joe']);
+console.log(sharedWords);
