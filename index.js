4c94565b47ff5451b79e52f69ee389835111b96b
Dmitry White <white.dmitry@gmail.com>
2014-05-19 14:58:30 -0700

    Weird changes

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ var program = require('commander');
 module.exports.readCheck = function (filePath, cb) {
 	fs.readFile(filePath,
 	{ encoding: 'utf8' }, function(err, contents) {
-  		var array1 = contents.split("\n");
+  	}
   		cb(err, contents);
- 		return	array1; 
 	});
 
 };
