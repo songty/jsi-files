029ea993f82d599310791217dde86336ae780df6
Dmitry White <white.dmitry@gmail.com>
2014-05-19 15:22:25 -0700

    Begin the compare function

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 var fs = require('fs');
 var program = require('commander');
 
-module.exports.readCheck = function (filePath, cb) {
+module.exports.readCheck = function (filePath) {
 	fs.readFile(filePath,
 	{ encoding: 'utf8' }, function(err, contents) {
-  	}
-  		cb(err, contents);
+  		console.log(contents);
 	});
 
 };
 
 module.exports.compare = function (array1, array2) {
-
+	var sharedWords = [];
+	var longer, shorter;
+	if (array1.length > array2.length) {
+		longer = array1;}
+	else {
+		longer = array2;
+	}
+	for (var i = 0; i < longer.length; i++) {
+		if (longer[i] === shorter[])
+	}
 };
